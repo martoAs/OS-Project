1c05a40497dc409717d797f0e5af5539de8110de
martoAs <martinaasteasuain22@gmail.com>
2023-10-30 15:35:40 -0300

    agrego archivo de aulas con mem compartida

---
+++ rAulasMemoria.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
+#define L 13
+#define cHilos 25
+
+
+
+int main(){
+
+
+
+}
+
