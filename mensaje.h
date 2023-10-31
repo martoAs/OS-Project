38658e50ab06a6993633540eb804a53765bde65d
guidoreale <realeguido1@gmail.com>
2023-10-31 01:26:20 -0300

    agregado bancoColaMensajes y modijicacion mensaje.h

--- mensaje.h
+++ mensaje.h
@@ -1,43 +1,18 @@
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#include <stdio.h>
-#include <string.h>
-#include <errno.h>
-
-typedef struct msgStruct{
-    long tipo;
-    int count;
-}tMsg;
-
-int recibirMensajeSinEspera(int tMensaje, int msgid){
-    tMsg mensaje;
-    if (msgrcv(msgid, &mensaje, sizeof(tMsg)-sizeof(long), tMensaje, IPC_NOWAIT)==-1){
-        if (errno != ENOMSG) {
-            printf("Error al acceder a la cola de mensajes: %s\n",strerror(errno));
-            exit(EXIT_FAILURE);
-        }else{
-            return -1;
-        }
-    }
-    return 1;
-}
-tMsg recibirMensaje(int tMensaje, int msgid){
-    tMsg mensaje;
-    if (msgrcv(msgid, &mensaje, sizeof(mensaje)-sizeof(long), tMensaje, 0)==-1){
-        printf("Error al acceder a la cola de mensajes: %s\n",strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-    return mensaje;
-}
-void enviarMensaje(int tMensaje, int msgid){
-    tMsg mensaje;
-    mensaje.tipo = tMensaje;
-    if (msgsnd(msgid, &mensaje, sizeof(mensaje)-sizeof(long), 0)==-1){
-        printf("Error al acceder a la cola de mensajes: %s\n",strerror(errno));
-        exit(EXIT_FAILURE);
-    }
-}
-}
+#define MAXIMO_MESA_ENTRADA 30
+#define MAXIMO_COLAS_PARTICULARES 15
+#define CANTIDAD_POLITICOS 5 //cantidad de politicos
+#define CANTIDAD_EMPRESAS 5 //cantidad de empresarios
+#define CANTIDAD_COMUNES 5  //cantidad de clientes comunes
+#define ENTRADA 100
+#define COLA_POLITICOS 101  //PRESI_LLENO
+#define POLITICO_ATENDIDO 102  //PRESI
+#define ESPERANDO_POLITICO 103    //ESP_PRESI
+#define COLA_EMPRESARIOS 104  //EMP_LLENO
+#define ESPERANDO_EMPRESARIO 105  //ESP_EMP
+#define EMPRESARIO_ATENDIDO 106  //EMP??
+#define COLA_CLIENTE_COMUN 107  //COM_LLENO
+#define COMUN_ESPERANDO 108  //ESP_COM
+#define COMUN_ATENDIDO 109   //COM
+#define DESPERTAR_EMPLEADO_COMUN 112 //DESPIERTO_COMUN
+#define DESPERTAR_EMPLEADO_EMPRESA 113 //DESPIERTO_EMPRESA
+#define MSGSIZE sizeof(char[1])
